674261b4f8fa496bd3ec38c0129c33be7a056b8b
gunshotop <94213144+gunshotop@users.noreply.github.com>
2023-04-09 15:33:28 +0530

    history hook used ot redirect to login page after clicking signout button on header

--- instaclone-189a5ca8a3d612ec968cdc6f49bbcb319925e0e2/src/components/header.js
+++ instaclone-189a5ca8a3d612ec968cdc6f49bbcb319925e0e2/src/components/header.js
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 import FirebaseContext from "../context/firebase"
 import UserContext from "../context/user"
 import * as ROUTES from "../constants/routes";
@@ -9,6 +9,7 @@ export default function Header() {
 
   const { firebase } = useContext(FirebaseContext);
   const { user } = useContext(UserContext);
+  const history = useHistory();
 
   console.log('user', user);
 
@@ -46,10 +47,13 @@ export default function Header() {
                 <button
                   type="button"
                   title="Sign out" 
-                  onClick={() => firebase.auth().signOut()}
+                  onClick={() => 
+                    {firebase.auth().signOut();
+                     history.push(ROUTES.LOGIN);}}
                   onKeyDown={(event) => {
                     if (event.key === 'Enter') {
-                        firebase.auth().signOut() 
+                        firebase.auth().signOut();
+                        history.push(ROUTES.LOGIN);
                     }
                   }}
                 >
